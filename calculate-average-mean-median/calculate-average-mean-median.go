bd5ee74b3d17d3cafaaf4b053439538ae14252d4
Guilherme Ferreira <guilhermeaferreira_t@yahoo.com.br>
2019-01-08 00:52:45 +0100

    examples of calculating mean & median in Go

---
+++ calculate-average-mean-median/calculate-average-mean-median.go
@@ -0,0 +1,101 @@
+package calculate_average_mean_median
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+// type
+type MeanMedian struct {
+	numbers []int
+}
+
+func main() {
+	// range of int numbers
+	mmType := MeanMedian{
+		numbers: []int{
+			//1671, 1817, 1763, 1733, 1722, 1745, 1773, 1778, 1725, 1696, 1689, 1718, 1735, 1705, 1734,
+			1, 2, 3, 4,
+		},
+	}
+
+	fmt.Printf("Sorted numbers:\t\t%v\n", mmType.numbers)
+	fmt.Println("------------------")
+	fmt.Printf("Minimum value:\t\t%v\n", mmType.GetMinValue())
+	fmt.Printf("Maximum value:\t\t%v\n", mmType.GetMaxValue())
+	fmt.Println("------------------")
+	fmt.Printf("Range of values:\t%v\n", mmType.CalcRangeValues())
+	fmt.Println("------------------")
+	fmt.Printf("Mean value:\t\t%v\n", mmType.CalcMean())
+	fmt.Println("------------------")
+	fmt.Printf("Median value:\t\t%v\n", mmType.CalcMedian())
+}
+
+func (mm *MeanMedian) Sort() {
+	sort.Ints(mm.numbers)
+}
+
+// return the minimum value
+func (mm *MeanMedian) GetMinValue() float64 {
+	mm.Sort() // sort the numbers
+
+	return float64(mm.numbers[0])
+}
+
+// return the maximum value
+func (mm *MeanMedian) GetMaxValue() float64 {
+	mm.Sort() // sort the numbers
+
+	return float64(mm.numbers[len(mm.numbers)-1])
+}
+
+// calculate the range values
+// last value - first value
+func (mm *MeanMedian) CalcRangeValues() float64 {
+	mm.Sort() // sort the numbers
+
+	return float64(mm.numbers[len(mm.numbers)-1]) - float64(mm.numbers[0])
+}
+
+// calculate the "mean" value
+// sum of all the int values
+// divided by its quantity
+func (mm *MeanMedian) CalcMean() float64 {
+	total := 0
+
+	for _, v := range mm.numbers {
+		total += v
+	}
+
+	// IMPORTANT: return was rounded!
+	return math.Round(float64(total) / float64(len(mm.numbers)))
+}
+
+// calculate the "median" value
+// if the total of numbers is odd
+// 	takes the middle value
+//
+// if the total of numbers is even
+//	calculate the "mean" of the middle two values
+func (mm *MeanMedian) CalcMedian(n ...int) float64 {
+	mm.Sort() // sort the numbers
+
+	mNumber := len(mm.numbers) / 2
+
+	if mm.IsOdd() {
+		return float64(mm.numbers[mNumber])
+	}
+
+	return float64(mm.numbers[mNumber-1]+mm.numbers[mNumber]) / float64(2)
+}
+
+// check if the total of numbers is
+// odd or even
+func (mm *MeanMedian) IsOdd() bool {
+	if len(mm.numbers)%2 == 0 {
+		return false
+	}
+
+	return true
+}
---
+++ calculate-average-mean-median/calculate-average-mean-median_test.go
@@ -0,0 +1,181 @@
+package calculate_average_mean_median
+
+import "testing"
+
+func TestMeanMedian_Sort(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		rightOrder []int
+	}{
+		{MeanMedian{
+			numbers: []int{
+				11, 3, 2, 5, 7, 1,
+			},
+		}, []int{1, 2, 3, 5, 7, 11}},
+	}
+
+	for _, table := range tables {
+		table.Sort()
+		for i, v := range table.numbers {
+			if v != table.rightOrder[i] {
+				t.Errorf("Sort() returned the wrong order %v, supposed to be %v.", table.numbers, table.rightOrder)
+			}
+		}
+	}
+}
+
+func TestMeanMedian_GetMinValue(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		min float64
+	}{
+		{MeanMedian{
+			numbers: []int{
+				13, 7, 11, 27, 3,
+			},
+		}, 3},
+	}
+
+	for _, table := range tables {
+		result := table.GetMinValue()
+		if result != table.min {
+			t.Errorf("GetMinValue() returned (%v) an incorrect response, supposed to be %v.", result, table.min)
+		}
+
+	}
+}
+
+func TestMeanMedian_GetMaxValue(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		max float64
+	}{
+		{MeanMedian{
+			numbers: []int{
+				13, 7, 11, 27, 3,
+			},
+		}, 27},
+	}
+
+	for _, table := range tables {
+		result := table.GetMaxValue()
+		if result != table.max {
+			t.Errorf("GetMaxValue() returned (%v) an incorrect response, supposed to be %v.", result, table.max)
+		}
+
+	}
+}
+
+func TestMeanMedian_CalcRangeValues(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		r float64
+	}{
+		{MeanMedian{
+			numbers: []int{
+				3, 4, 2, 6,
+			},
+		}, 4},
+	}
+
+	for _, table := range tables {
+		result := table.CalcRangeValues()
+		if result != table.r {
+			t.Errorf("CalcRangeValues() returned (%v) an incorrect response, supposed to be %v.", result, table.r)
+		}
+
+	}
+}
+
+func TestMeanMedian_CalcMean(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		r float64
+	}{
+		{MeanMedian{
+			numbers: []int{
+				4, 4, 2, 2,
+			},
+		}, 3},
+	}
+
+	for _, table := range tables {
+		result := table.CalcMean()
+		if result != table.r {
+			t.Errorf("CalcMean() returned (%v) an incorrect response, supposed to be %v.", result, table.r)
+		}
+
+	}
+}
+
+func TestMeanMedian_CalcMedianOdd(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		r float64
+	}{
+		{MeanMedian{
+			numbers: []int{
+				3, 4, 2, 6, 5,
+			},
+		}, 4},
+	}
+
+	for _, table := range tables {
+		result := table.CalcMedian()
+		if result != table.r {
+			t.Errorf("CalcMedian() (with odd) returned (%v) an incorrect response, supposed to be %v.", result, table.r)
+		}
+
+	}
+}
+
+func TestMeanMedian_CalcMedianEven(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		r float64
+	}{
+		{MeanMedian{
+			numbers: []int{
+				3, 4, 2, 6,
+			},
+		}, 3.5},
+	}
+
+	for _, table := range tables {
+		result := table.CalcMedian()
+		if result != table.r {
+			t.Errorf("CalcMedian() (with even) returned (%v) an incorrect response, supposed to be %v.", result, table.r)
+		}
+
+	}
+}
+
+func TestMeanMedian_IsOdd(t *testing.T) {
+	tables := []struct {
+		MeanMedian
+		r bool
+	}{
+		{MeanMedian{
+			numbers: []int{
+				1, 2,
+			},
+		}, false},
+		{MeanMedian{
+			numbers: []int{
+				1, 1, 1, 4,
+			},
+		}, false},
+		{MeanMedian{
+			numbers: []int{
+				1, 1, 1, 1, 5,
+			},
+		}, true},
+	}
+
+	for _, table := range tables {
+		result := table.IsOdd()
+		if result != table.r {
+			t.Errorf("isOdd() returned (%v) an incorrect response: %v, supposed to be %v.", result, table.MeanMedian.numbers, table.r)
+		}
+	}
+}
